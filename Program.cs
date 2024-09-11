535d0d9a694c4dfe1808c437cf17dd17f77ea932
Anirudha Gupta <35772421+anirugu@users.noreply.github.com>
2024-09-11 00:32:47 +0000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+public class Solution {
+    public int LastStoneWeight(int[] stones) {
+        if(stones.Length == 0)
+            return 0;
+        while(stones.Length > 1){
+            Array.Sort(stones);
+            var remain = stones[stones.Length-1] - stones[stones.Length-2];
+            stones[stones.Length-2] = remain;
+            Array.Resize(ref stones, stones.Length-1);
+        }
+        return stones[stones.Length-1];
+    }
+}
